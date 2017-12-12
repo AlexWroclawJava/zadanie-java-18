6337c4e304a0072f2d7548f0bd1ee6ddf5962f80
AlexWroclawJava <aleksander.szlachcic@gmail.com>
2017-12-12 01:46:12 +0100

    zadanie-java-18 edit

--- src/ArrayComparator.java
+++ src/ArrayComparator.java
@@ -5,17 +5,17 @@ public class ArrayComparator {
 
     public boolean compare(int tablica1[][], int tablica2[][]) {
         if (tablica1.length == tablica2.length) {
-            n++;
+        n++;
         }
         for (int i = 0; i < tablica1.length; i++) {
                 if (tablica1[i].length == tablica2[i].length) {
-                    n++;
+                n++;
                 }
             }
             for (int i = 0; i < tablica1.length; i++) {
                         for (int j = 0; j < tablica1[i].length ; j++) {
                             if (tablica1[i][j] == tablica2[i][j]) {
-                                n++;
+                            n++;
                         }
                 }
             }
